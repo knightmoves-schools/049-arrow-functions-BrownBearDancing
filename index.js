992fcf8700ea5aace9bc432bf7ee41d3f101d4b9
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-11-13 14:33:42 -0600

    is this correct

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
+let add = (a,b) => a + b;
+document.getElementById("result").innerHTML = add(2,3);
 
+should create a variable named add and assign it an arrow function that takes two number parameters and returns their sum
